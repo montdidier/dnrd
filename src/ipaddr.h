1b562bafd6811c73ed30f6a8a29d4df0390f3c23
Chris Kruger <montdidier@users.noreply.github.com>
2020-02-07 22:53:47 +0800

    added license preamble

--- src/ipaddr.h
+++ src/ipaddr.h
@@ -1,3 +1,22 @@
+/*
+    File: ipaddr.h - helper code for IPv4-mapped IPv6 addresses
+
+    Copyright (C) 2020 by Chris Kruger <montdidier@users.noreply.github.com>
+
+    This source is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2, or (at your option)
+    any later version.
+
+    This source is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #ifndef _DNRD_IPADDR_H_
 #define _DNRD_IPADDR_H_
 
